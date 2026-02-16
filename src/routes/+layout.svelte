1a9b557f6e9cee0db0b2d122fc92b8d1ee710ffc
Yusuke Oshiro <yusuke@oshiro1.com>
2026-02-16 04:29:22 +0000

    chore: Replace SVG favicon with PNG favicon.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import { onMount } from 'svelte';
 	import { onAuthStateChanged } from 'firebase/auth';
 	import { auth } from '$lib/firebase';
